24b7a7c351bd5455af040179f4957e7dfb5472e4
João Pedro <126481770+JoaopedroHolanda@users.noreply.github.com>
2025-07-16 16:10:51 -0300

    feat: criando projeto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,30 @@
+import Head from 'next/head';
 import styles from '../../styles/home.module.css';
+import Image from 'next/image';
+
+import heroImg from "../../public/assets/hero.png"
 
 export default function Home() {
-  return (
-    
+  return(
+    <div>
+      <Head>
+        <title>tarefas+ | Organize suas tarefas de forma fácil</title>
+      </Head>
+
+      <main className={styles.main}>
+        <div className={styles.logoContent}>
+          <Image 
+            className={styles.hero}
+            alt="tarefas+ logo"
+            src={heroImg}
+            priority
+          />
+        </div>
+
+        <h1 className={styles.title}>
+          Sistema feito para você organizar <br/> seus estudos e tarefas
+        </h1>
+      </main>
+    </div>
   );
 }
