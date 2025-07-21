524406dd649c8b415017e4790a9d90acc2049aed
João Pedro <126481770+JoaopedroHolanda@users.noreply.github.com>
2025-07-21 20:16:44 -0300

    feat: implementando secção "minhas tarefas"

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -4,12 +4,15 @@ import Head from 'next/head'
 
 import { getSession } from 'next-auth/react'
 import { Textarea } from '../../components/textarea'
+import { FiShare2 } from 'react-icons/fi'
+import { FaTrash } from 'react-icons/fa'
+
 
 export default function Dashboard(){
     return(
         <div className={styles.container}>
             <Head>
-                <title>Meu painel de tarrefas</title>
+                <title>Tarefas+: Painel de Tarefas</title>
             </Head>
 
             <main className={styles.main}>
@@ -25,6 +28,29 @@ export default function Dashboard(){
                                 <button type="submit" className={styles.button}>Registrar</button>
                             </form>
                     </div>
+                </section>
+
+                <section className={styles.taskContainer}>
+                    <h1>Minhas tarefas</h1>
+
+                    <article className={styles.task}>
+                        <div className={styles.tagContainer}>
+                            <label className={styles.tag}>PUBLICO</label>
+                            <button className={styles.shareButton}>
+                                <FiShare2
+                                    size={20}
+                                    color="#3183ff"
+                                />
+                            </button>
+                        </div>
+
+                        <div className={styles.taskContent}>
+                            <p>Descrição da tarefa</p>
+                            <button className={styles.trashButton}>
+                                <FaTrash size={20} color="#ff4d4d" />
+                            </button>
+                        </div>
+                    </article>  
                 </section>
             </main>
         </div>
